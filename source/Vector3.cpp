08d9894bdf883f33f7108b2cc40e63f78e5b1643
王栋 <dong.wang@appshahe.com>
2021-12-12 21:42:34 +0800

    创建vector3和matrix3类

--- source/Vector3.cpp
+++ source/Vector3.cpp
@@ -1,1 +1,47 @@
 #include "Vector3.h"
+
+Vector3 Vector3::operator+(const Vector3& value) const
+{
+	return { x + value.x,y + value.y,z + value.z };
+}
+
+Vector3 Vector3::operator-(const Vector3& value) const
+{
+	return { x - value.x,y - value.y,z - value.z };
+}
+
+float Vector3::operator*(const Vector3& value) const
+{
+	return x * value.x + y * value.y + z * value.z;
+}
+
+bool Vector3::operator==(const Vector3& value) const
+{
+	return x == value.x && y == value.y && z == value.z;
+}
+
+bool Vector3::operator!=(const Vector3& value) const
+{
+	return !operator==(value);
+}
+
+Vector3 Vector3::CrossProduct(const Vector3& value)
+{
+	return { x * value.y - y * value.x,y * value.z - z * value.y,z * value.x - x * value.z };
+}
+
+Vector3& Vector3::operator+=(const Vector3& value)
+{
+	x += value.x;
+	y += value.y;
+	z += value.z;
+	return *this;
+}
+
+Vector3& Vector3::operator-=(const Vector3& value)
+{
+	x -= value.x;
+	y -= value.y;
+	z -= value.z;
+	return *this;
+}
